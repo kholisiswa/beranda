9515b5fef3021589c7c82b422210d95b0421e13c
kholisiswa <kholis.is26@gmail.com>
2024-10-29 00:06:49 +0700

    Add files via upload

---
+++ kopi.js
@@ -0,0 +1,4 @@
+// Basic script placeholder, can be extended with animations or interactions
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Robusta Coffee Shop Website Loaded!');
+});
